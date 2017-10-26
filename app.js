26f7624547beded02399ea3bbb2cc7ccbde6001b
tiffywang92 <tiffywang92@yahoo.com>
2017-10-26 11:20:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -44,11 +44,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
